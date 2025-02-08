773390dba26612bd3e2ff9e41eefcf6fc11c6485
dineshdb <dineshdb@dbhattarai.info.np>
2025-02-09 00:47:16 +0545

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,10 +12,10 @@ RELEASE="$(rpm -E %fedora)"
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-dnf remove -y gnome-tour gnome-software
+#dnf remove -y gnome-tour gnome-software firefox firefox-langpacks
 
 # dev tools
-dnf install -y zsh podman-docker openssl git-absorb
+dnf install -y zsh podman-docker openssl
 
 # basic utilites
 dnf install -y syncthing powertop vulkan-tools tailscale wl-clipboard
@@ -26,8 +26,6 @@ dnf install -y gpaste gnome-shell-extension-gpaste gnome-shell-extension-blur-my
 dnf install -y pipewire-v4l2
 
 dnf install -y pritunl-client
-
-dnf install -y flatpak-builder
 
 # add cosmic-desktop because I want to test run it
 dnf install -y cosmic-ext-applet-clipboard-manager cosmic-desktop
